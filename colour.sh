b87cf10c47cd0dca2297748e916285bd996a9b63
sridharreddyaileni <sridharreddy2508@gmail.com>
2025-01-06 12:22:52 +0530

    myscript

--- colour.sh
+++ colour.sh
@@ -16,4 +16,4 @@ else
     echo "$G My $Y Name is $R Sridhar $N " &>> $LOGFILE
     echo "script executed at $TIMESTAMP" &>> $LOGFILE
 fi
-cp mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
+cp mongo.repo /etc/mongo &>> $LOGFILE
