4767cdd994c61bb8f5fed4dfb8d26c5f7b4f1219
sridharreddyaileni <sridharreddy2508@gmail.com>
2025-01-06 11:45:02 +0530

    myscript

--- colour.sh
+++ colour.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
-
+ID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
 TIMESTAMP=$(date +F-%H-%M-%S)
-echo "$G My $Y Name is $R Sridhar $N "
-echo "script executed at $TIMESTAMP"
+
+if [$ID -ne 0]
+then
+    echo "$G My $Y Name is $R Sridhar $N "
+    echo "script executed at $TIMESTAMP"
+else
+    echo "you are root user"
+fi
