66fbb4105ac86c2d1433bc49691ad57f18da362e
sridharreddyaileni <sridharreddy2508@gmail.com>
2025-01-06 12:01:48 +0530

    myscript

--- colour.sh
+++ colour.sh
@@ -6,10 +6,11 @@ Y="\e[33m"
 N="\e[0m"
 
 TIMESTAMP=$(date +F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
 if [ $ID -ne 0 ]
 then
-    echo "$R ERROR:: Please run this script with root access $N"
+    echo "$R ERROR:: Please run this script with root access $N" &>> $LOGFILE
 else
     echo "$G you are root user $N"
     echo "$G My $Y Name is $R Sridhar $N "
