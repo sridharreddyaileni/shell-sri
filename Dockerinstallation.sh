7766407837f198196931e4b6ae2d294cae1548d4
sridharreddyaileni <sridharreddy2508@gmail.com>
2025-01-13 12:47:44 +0530

    dockerinstall

--- Dockerinstallation.sh
+++ Dockerinstallation.sh
@@ -9,3 +9,5 @@ sudo chmod a+r /etc/apt/keyrings/docker.asc
 # Add the repository to Apt sources:
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo docker run hello-world
